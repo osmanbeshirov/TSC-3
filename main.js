955add7cf30b667a307fc9a84cb8ccf51a249c40
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-20 03:23:32 +0300

    basics types of TS

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-var sayHello = function (message) {
-    console.log(message);
-};
-sayHello('Hi');
+// const sayHello = (message: string) => {
+//     console.log(message)
+// }
+// sayHello('Hi')
+// Types in TS
+// 1. Any
+// 2. Number
+// 3. String
+// 4. Boolean
+// 5. Arrays string[], number[], any[]
+// 6. Enum
+// 7. Tuple[type1, type2]
+// 8. Unknown
+// 9. TYPE INFERENCE ?
+// let a: any;
+// a = 5;
+// a = '5'
+// a = true;
+// let x: string;
+// x = 'HI Osman';
+// let y: number;
+// y = 12;
+// y = 30;
+// let z: boolean;
+// z = true;
+// z = false;
+var myStrArray;
+myStrArray = ['a', 'b', 'c'];
--- main.ts
+++ main.ts
@@ -1,5 +1,37 @@
-const sayHello = (message: string) => {
-    console.log(message)
-}
+// const sayHello = (message: string) => {
+//     console.log(message)
+// }
 
-sayHello('Hi')
+// sayHello('Hi')
+
+// Types in TS
+// 1. Any
+// 2. Number
+// 3. String
+// 4. Boolean
+// 5. Arrays string[], number[], any[]
+// 6. Enum
+// 7. Tuple[type1, type2]
+// 8. Unknown
+// 9. TYPE INFERENCE ?
+
+// let a: any;
+// a = 5;
+// a = '5'
+// a = true;
+
+// let x: string;
+// x = 'HI Osman';
+
+// let y: number;
+// y = 12;
+// y = 30;
+
+// let z: boolean;
+// z = true;
+// z = false;
+
+let myStrArray: string[];
+myStrArray = ['a', 'b', 'c']
+
+
